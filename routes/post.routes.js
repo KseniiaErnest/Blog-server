221143841426d3403522f6b1358e5117cdc01136
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-09-28 11:57:49 -0400

    Wokring on post routes

---
+++ routes/post.routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const Post = require('../models/Post.model');
+const Comment = require('../models/Comment.model');
+
+// Create
+router.post('/post', (req, res, next) => {
+  // const { title, content, author, publicationDate, comments, likes, images } = req.body;
+  const postData = {...req.body};
+
+  Post.create(postData)
+  .then((post) => {
+    res.json({ success: true, post});
+  })
+  .catch((err) => {
+    res.json({ success: false, error: err});
+  })
+
+
+});
