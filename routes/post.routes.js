e3216d749a10349586a0d41859e97e5ecd841003
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-09-29 13:05:05 -0400

    Post CRUD basic done

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -6,19 +6,54 @@ const Post = require('../models/Post.model');
 const Comment = require('../models/Comment.model');
 
 // Create
-router.post('/post', (req, res, next) => {
-  // const { title, content, author, publicationDate, comments, likes, images } = req.body;
-  const postData = {...req.body};
-
-  Post.create(postData)
-  .then((post) => {
-    res.json({ success: true, post});
-  })
-  .catch((err) => {
-    res.json({ success: false, error: err});
-  })
-
-
+router.post('/post', async (req, res, next) => {
+  try {
+    const postData = {...req.body};
+    const postToCreate = await Post.create(postData);
+    res.json({ success: true, postToCreate });
+  } catch(err) {
+    res.json({ success: false, error: err });
+  }
 });
+
+// Read all
+router.get('/post', async (req, res, next) => {
+  try {
+    const postToReadAll = await Post.find();
+    res.json({ success: true, postToReadAll });
+  } catch(err) {
+    res.json({ succes: false, error: err });
+  }
+});
+
+// Read one
+router.get('/post/:postId', async (req, res, next) => {
+  try {
+    const postToReadOne = await Post.findById(req.params.postId);
+    res.json({ succes: true, postToReadOne });
+  } catch(err) {
+    res.json({ succes: false, error: err });
+  }
+})
+
+// Update
+router.put('/post/:postId', async (req, res, next) => {
+  try {
+    const postToUpdate = await Post.findByIdAndUpdate(req.params.postId, req.body, { new: true });
+    res.json({ success: true, postToUpdate });
+  } catch (err) {
+    res.json({ success: false, error: err });
+  }
+});
+
+// Delete
+router.delete('/post/:postId', async (req, res, next) => {
+  try {
+    const postToDelete = await Post.findByIdAndRemove(req.params.postId);
+    res.json({ succes: true, postToDelete });
+  } catch (err) {
+    res.json({ succes: false, error: err });
+  }
+})
 
 module.exports = router;
